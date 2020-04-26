016f1fbdf6aa55507bfd9ca0ee5bd622ea8da5c1
mehrad-19 <63733140+mehrad-19@users.noreply.github.com>
2020-04-26 22:25:10 +0430

    Add files via upload

---
+++ E7.py
@@ -0,0 +1,9 @@
+print('E7_color')
+print('enter your favorite color :')
+C = str(input('your color:'))
+if C=='red' or C=='RED' or C=='Red' :
+    print(" I like red too .")
+else :
+    
+    
+    print(" I don't like "+C+",I prefer red. ")
