e466fe7d3ec832b9b0c0b0dc3186c079d069c5a7
mehrad-19 <63733140+mehrad-19@users.noreply.github.com>
2020-04-28 02:55:28 +0430

    Delete E7.py

--- E7.py
+++
@@ -1,9 +0,0 @@
-print('E7_color')
-print('enter your favorite color :')
-C = str(input('your color:'))
-if C=='red' or C=='RED' or C=='Red' :
-    print(" I like red too .")
-else :
-    
-    
-    print(" I don't like "+C+",I prefer red. ")
